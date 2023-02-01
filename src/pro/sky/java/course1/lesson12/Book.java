f774469f8cd2419b8a40677d8295facbbcf3083f
irina <irishkapos16@gmail.com>
2023-02-01 23:00:35 +0300

    Homework13

--- src/pro/sky/java/course1/lesson12/Book.java
+++ src/pro/sky/java/course1/lesson12/Book.java
@@ -1,20 +1,26 @@
 package pro.sky.java.course1.lesson12;
 
-public class Book{
-    private String title;
-    private Author author;
+import java.util.Objects;
+
+public class Book {
+    private final String title;
+    private final Author author;
     private int publicationYear;
+
     public Book(String title, Author author, int publicationYear) {
-        this.title = title ;
+        this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
     }
+
     public Book(Author author) {
-        this("No Title",author,0);
+        this("No Title", author, 0);
     }
+
     public String getTitle() {
         return title;
     }
+
     public Author getAuthor() {
         return author;
     }
@@ -26,5 +32,25 @@ public class Book{
 
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги - " + title + " , " + author.toString() + " , год публикации - " + publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title)
+                && Objects.equals(author, book.author)
+                && Objects.equals(publicationYear, book.publicationYear);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.title , this.author, this.publicationYear);
     }
 }
